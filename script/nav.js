c73cbd05d0e0bb2697e44787f951e2661810fc2a
DerizhyLouise <louise.derizhy@gmail.com>
2024-06-04 00:28:10 +0700

    feat: login logic and session implementation fix: ui for login and registration

--- script/nav.js
+++ script/nav.js
@@ -129,7 +129,7 @@ const showNavbar = () => {
                         </ul>
                     </li>
                     <li>
-                        <a href="./auth/login.html"
+                        <a href="./login.html"
                             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-green-500 group">
                             <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
                                 aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
@@ -140,7 +140,7 @@ const showNavbar = () => {
                         </a>
                     </li>
                     <li>
-                        <a href="./auth/register.html"
+                        <a href="./register.html"
                             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-green-500 group">
                             <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
                                 aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
